0402182b8c01502fb8eecf216a3cff5ab8c8bed3
Frank Matranga <thefrankmatranga@gmail.com>
2016-02-05 22:25:20 -0500

    Big improvements

--- scraper.rb
+++ scraper.rb
@@ -26,14 +26,14 @@ page = @agent.post('https://moodle.regis.org/login/index.php', {
 })
 
 if page.title != "Dashboard"
-	puts "Failed to login!" 
+	puts "Failed to login!"
 	exit
 end
 
 @client = Mysql2::Client.new(:host => secrets['DB_HOST'], :username => secrets['DB_USER'], :password => secrets['DB_PASSWORD'], :database => secrets['DB_NAME'])
 
 sql = <<-SQL
-CREATE TABLE IF NOT EXISTS COURSES (
+CREATE TABLE IF NOT EXISTS courses (
 	ID INT NOT NULL,
 	TEACHER_MID INT,
 	TITLE VARCHAR(50) NOT NULL,
@@ -43,36 +43,47 @@ SQL
 @client.query(sql)
 
 sql = <<-SQL
-CREATE TABLE IF NOT EXISTS STAFF (
+CREATE TABLE IF NOT EXISTS staffs (
 	ID INT NOT NULL,
 	USERNAME VARCHAR(30) NOT NULL,
 	FIRST_NAME VARCHAR(15) NOT NULL,
 	LAST_NAME VARCHAR(30) NOT NULL,
 	DEPARTMENT VARCHAR(20),
-	MPICTIURE VARCHAR(100),
+	MPICTURE VARCHAR(100),
 	PRIMARY KEY (ID)
 );
 SQL
 @client.query(sql)
 
 sql = <<-SQL
-CREATE TABLE IF NOT EXISTS STUDENTS (
+CREATE TABLE IF NOT EXISTS students (
 	ID INT NOT NULL,
 	USERNAME VARCHAR(30) NOT NULL,
 	FIRST_NAME VARCHAR(15) NOT NULL,
 	LAST_NAME VARCHAR(30) NOT NULL,
 	ADVISEMENT VARCHAR(20),
-	MPICTURE VARCHAR(100)
+	MPICTURE VARCHAR(100),
 	PRIMARY KEY (ID)
 );
 SQL
-#@client.query(sql)
+@client.query(sql)
 
-@grades = Hash.new
-@grades[1] = 19
-@grades[2] = 18
-@grades[3] = 17
-@grades[4] = 16
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS students_courses (
+	ID INT NOT NULL AUTO_INCREMENT,
+	student_id INT NOT NULL,
+	course_id INT NOT NULL,
+	PRIMARY KEY (ID)
+);
+SQL
+@client.query(sql)
+
+@grades = {
+	"1" => 19,
+	"2" => 18,
+	"3" => 17,
+	"4" => 16
+}
 
 def extract_person(mid, page)
 	name = page.title.split(":")[0].split(" ")
@@ -81,46 +92,59 @@ def extract_person(mid, page)
 	if name.length > 2
 		last_name = name[1] + " " + name[2]
 	end
-	
+
 	picture = page.search("a/img[@alt=\"Picture of #{first_name} #{last_name}\"]")[0]['src']
 	type = :staff
-	
+
 	department = page.search("//dd[../dt = 'Department']/text()").to_s
 	if /\A\d+\z/.match(department[0])
 		type = :student
 	end
-	
+
 	puts "#{mid.to_s}: #{type.capitalize} #{last_name}, #{first_name} of #{department}"
-	
+
 	if type == :staff
 		username = (first_name[0] + last_name).downcase
-		statement = @client.prepare("INSERT INTO STAFF VALUES(?, ?, ?, ?, ?, ?)")
-		statement.execute(mid, username, first_name, last_name, department, picture)
+		sql = "INSERT INTO staffs (ID, USERNAME, FIRST_NAME, LAST_NAME, DEPARTMENT, MPICTURE) VALUES(#{mid}, \"#{username}\", \"#{first_name}\", \"#{last_name}\", \"#{department}\", \"#{picture}\")"
+		sql += " ON DUPLICATE KEY UPDATE USERNAME=\"#{username}\", FIRST_NAME=\"#{first_name}\", LAST_NAME=\"#{last_name}\", DEPARTMENT=\"#{department}\", MPICTURE=\"#{picture}\""
+		puts sql
+		@client.query(sql)
 	else
-		username = (first_name[0] + last_name + @grades[department[0]]).downcase
+		grade =  @grades[department[0]]
+		username = (first_name[0] + last_name + grade.to_s).downcase
+		sql = "INSERT INTO students (ID, USERNAME, FIRST_NAME, LAST_NAME, ADVISEMENT, MPICTURE) VALUES(#{mid}, \"#{username}\", \"#{first_name}\", \"#{last_name}\", \"#{department}\", \"#{picture}\")"
+		sql += " ON DUPLICATE KEY UPDATE USERNAME=\"#{username}\", FIRST_NAME=\"#{first_name}\", LAST_NAME=\"#{last_name}\", ADVISEMENT=\"#{department}\", MPICTURE=\"#{picture}\""
+		@client.query(sql)
+
+		@client.query("DELETE FROM students_courses WHERE student_id=#{mid}")
+		page.search("//dd/ul/li/a[contains(@href, 'http://moodle.regis.org/course/view.php?id=')]").each do |link|
+			cid = link["href"].split("id=")[1].split("&")[0]
+			sql = "INSERT INTO students_courses (student_id, course_id) VALUES (#{mid}, #{cid})"
+			@client.query(sql)
+		end
 	end
 end
 
 
 def extract_course(mid, page)
 	parts = page.title.split(":")
-	
+
 	title = parts[1]
 	if parts.length == 1
 		title = parts[0]
 	end
-	
+
 	title.strip!
-	
+
 	teacher_page = @agent.get("http://moodle.regis.org/user/index.php?roleid=3&sifirst=&silast=&id="+mid.to_s)
-	
+
 	teacher_id = nil
 	begin
 		teacher_id = teacher_page.search("//strong/a[contains(@href, 'moodle.regis.org')]")[0]['href'].split("?id=")[1].split("&course=")[0].to_i
 	rescue NoMethodError
-		
+
 	end
-	
+
 	puts mid.to_s + ": " + title
 	title = @client.escape(title)
 	t_id = "NULL"
@@ -129,13 +153,13 @@ def extract_course(mid, page)
 		t_id = teacher_id
 		t_id2 = "TEACHER_MID=#{teacher_id}, "
 	end
-	
-	sql = "INSERT INTO COURSES VALUES(#{mid}, #{t_id}, '#{title}') ON DUPLICATE KEY UPDATE #{t_id2}TITLE='#{title}'"
-	puts sql
+
+	sql = "INSERT INTO courses VALUES(#{mid}, #{t_id}, '#{title}') ON DUPLICATE KEY UPDATE #{t_id2}TITLE='#{title}'"
 	@client.query(sql)
 end
 
 
+=begin
 (1..700).each do |i|
 	sleep(1)
 	begin
@@ -149,18 +173,20 @@ end
 		next
 	end
 end
+=end
 
-
-(1..3000).each do |i|
+(1198..3000).each do |i|
 	sleep(1)
 	begin
 		page = @agent.get("http://moodle.regis.org/user/profile.php?id=" + i.to_s)
 		if page.title == "Notice" or page.title == "Error" or !page.title
-			@client.query("DELETE FROM STAFF WHERE ID=#{i}")
-			#@client.query("DELETE * FROM STUDENTS WHERE MID=#{i}")
+			@client.query("DELETE FROM staffs WHERE ID=#{i}")
+			@client.query("DELETE FROM students WHERE ID=#{i}")
 		end
 		extract_person(i, page)
 	rescue Exception => e
-		puts e
+		puts "Skipped #{i}"
+		#puts e.inspect
+		#puts e.backtrace.join("\n")
 	end
 end
